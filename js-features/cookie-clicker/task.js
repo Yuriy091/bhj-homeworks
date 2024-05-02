8824abc17f228115c43b192819e7e243cccff025
YuriiS <yu.syury@mail.ru>
2024-05-02 22:32:04 +0300

    первая фиксация

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,9 +1,24 @@
 let clicker = document.getElementById('clicker__counter');
 let cookie = document.getElementById('cookie');
 let counter = 0;
+const img = document.querySelector('img')
 
 cookie.addEventListener('click', function(counterClick) {
     counter++;
     clicker.textContent = counter;
+    console.log('click');
+
+    
 })
+
+    
+
+    img.addEventListener('mousedown', ()=> {
+        img.width = 250;
+    });
+
+    img.addEventListener('mouseup', ()=> {
+        img.width = 200;
+    });
+   
 
