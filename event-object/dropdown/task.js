6797547245704b2ba5ece86532e0a3627579577f
YuriiS <yu.syury@mail.ru>
2024-05-10 17:06:10 +0300

    first commit dropdown

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const list = document.querySelector('.dropdown__list');
     const valueView = document.querySelector('.dropdown__value');
-
+// 
     valueView.addEventListener('click', openDropdown);
 
     const links = Array.from(list.querySelectorAll('.dropdown__link'));
